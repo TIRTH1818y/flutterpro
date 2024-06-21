92decc058358c339285b421ac8090d7bc5bbabb9
SONIGARA TIRTH <153047795+TIRTH1818y@users.noreply.github.com>
2024-06-21 15:37:23 +0530

    Update main.dart
    name change

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(  backgroundColor: Colors.teal,
-          title: Text("IT"),
+          title: Text("tanjiro kamado"),
 
           leading: Builder(
             builder: (context) {
