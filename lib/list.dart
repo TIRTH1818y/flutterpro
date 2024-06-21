636eb989c5c097f40a152af2f5ea4fef83d32bba
SONIGARA TIRTH <153047795+TIRTH1818y@users.noreply.github.com>
2024-06-21 15:38:49 +0530

    new change

---
+++ lib/list.dart
@@ -0,0 +1,71 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(  backgroundColor: Colors.teal,
+          title: Text("tanjiro kamado"),
+
+
+
+      ),
+
+
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.teal
+      ),
+
+
+
+      body:
+      Text("hello list")
+      ,
+
+
+
+    );
+  }
+}
